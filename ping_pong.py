c866c23ecd5f1147e9daa4460a25d0e1b575aa21
suvorovaeva <147901993+suvorovaeva@users.noreply.github.com>
2023-10-20 19:10:52 +0500

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -4,22 +4,18 @@ from pygame import *
 #класс-родитель для других спрайтов
 class GameSprite(sprite.Sprite):
 #конструктор класса
-    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
-      #вызываем конструктор класса (Sprite):
-        sprite.Sprite.__init__(self)
-      #каждый спрайт должен хранить свойство image - изображение
-        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (wight, height))
         self.speed = player_speed
-      #каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
-#метод, отрисовывающий героя на окне
+        
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
-#класс главного игрока
+        
 class Player(GameSprite):
-  #метод для управления спрайтом стрелками клавиатуры
     def update_r(self):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
